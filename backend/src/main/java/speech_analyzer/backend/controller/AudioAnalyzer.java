11d0a9f3126d669d29b41e55c6838f601272b588
Huzaifa <106070061+huzaifa1221@users.noreply.github.com>
2025-08-27 00:35:28 +0530

    Revamp (#11)
    * new delpoy
    
    * new delpoy
    
    * new delpoy
    
    * added healthcheck

--- backend/src/main/java/speech_analyzer/backend/controller/AudioAnalyzer.java
+++ backend/src/main/java/speech_analyzer/backend/controller/AudioAnalyzer.java
@@ -3,6 +3,7 @@ package speech_analyzer.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,11 @@ public class AudioAnalyzer {
             return new ResponseEntity<SpeechAnalysisDto>(response, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "healthy";
     }
 }
 
